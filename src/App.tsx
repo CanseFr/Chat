a13ddc3757759b6a92a4c28ecfd2bd017f24d200
CanseFr <jucansell@gmail.com>
2024-03-11 23:04:25 +0100

    :sparkles: Setup route provider

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,7 @@
 import {Container, createTheme, CssBaseline, ThemeProvider} from '@mui/material'
 import {Auth} from "./components/auth/auth";
+import {RouterProvider} from "react-router-dom";
+import router from "./routes";
 
 const darkTheme = createTheme({
   palette: {
@@ -12,7 +14,7 @@ const App = () => {
     <ThemeProvider theme={darkTheme}>
       <CssBaseline/>
       <Container>
-        <Auth/>
+        <RouterProvider router={router}/>
       </Container>
     </ThemeProvider>
   );

f340dfc0bfe38031513437631948b38f5bbdf141
CanseFr <jucansell@gmail.com>
2024-03-13 23:23:10 +0100

    :sparkles: Setup signup call

--- src/components/auth/signup.tsx
+++ src/components/auth/signup.tsx
@@ -1,10 +1,22 @@
 import {Auth} from "./auth";
 import {Link} from "react-router-dom";
 import {Link as MUILink} from "@mui/material"
+import {useCreateUser} from "../../hooks/useCreateUser";
 
 const Signup = () => {
-  return<Auth submitLabel="Signup" onSubmit={async ()=>{}}>
-    <Link to={'/login'} style={{ alignSelf: "center"}}>
+  const [createUser] = useCreateUser();
+
+  return <Auth submitLabel="Signup" onSubmit={async ({email, password}) => {
+    await createUser({
+      variables: {
+        createUserInput: {
+          email,
+          password
+        }
+      }
+    })
+  }}>
+    <Link to={'/login'} style={{alignSelf: "center"}}>
       <MUILink>Login</MUILink>
     </Link>
   </Auth>
